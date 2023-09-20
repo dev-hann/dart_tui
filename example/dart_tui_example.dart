a82b6147b1c52f112f3d2056d2e4c87b29b4cbe4
dev-hann <developer.hann@gmail.com>
2023-09-20 16:49:39 +0800

    :tada: Begin Project

---
+++ example/dart_tui_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_tui/dart_tui.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_tui.dart
@@ -0,0 +1,3 @@
+library;
+
+export './src/tui.dart';
